f8d4a89745436ab03f10ff94e83c1129077e3153
DoGiaAn <anblcc2002@gmail.com>
2022-06-27 12:05:07 +0700

    Add files via upload

---
+++ BTL_Mo_Hinh_Phan_Mem_Nhom12/src/Model/CauHoi.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class CauHoi {
+    public String MSSV;
+    public String NgayDat;
+    public String NoiDung;
+
+    public CauHoi() {
+
+    }
+
+    public CauHoi(String MSSV, String NgayDat, String NoiDung) {
+        this.MSSV = MSSV;
+        this.NgayDat = NgayDat;
+        this.NoiDung = NoiDung;
+    }
+}
---
+++ BTL_Mo_Hinh_Phan_Mem_Nhom12/src/Model/CauTraLoi.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class CauTraLoi {
+    public String MSSV;
+    public String NgayTraLoi;
+    public String NoiDung;
+
+    public CauTraLoi() {
+
+    }
+
+    public CauTraLoi(String MSSV, String NgayTraLoi, String NoiDung) {
+        this.MSSV = MSSV;
+        this.NgayTraLoi = NgayTraLoi;
+        this.NoiDung = NoiDung;
+    }
+}
